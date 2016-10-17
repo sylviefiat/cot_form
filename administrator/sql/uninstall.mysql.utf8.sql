9e335d081ce8221e0d5cf195a8a6166bacecb2c9
sylviefiat <sylvie.fiat@ird.fr>
2016-10-17 15:29:50 +1100

    reinit

--- administrator/sql/uninstall.mysql.utf8.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE IF EXISTS `#__cot_admin`;
-DROP TABLE IF EXISTS `#__cot_public`;
-DROP TABLE IF EXISTS `#__sc_admin`;
-DROP TABLE IF EXISTS `#__tabu_admin`;
-DROP TABLE IF EXISTS `#__cbm_admin`;
-DROP TABLE IF EXISTS `#__habitat_admin`;
